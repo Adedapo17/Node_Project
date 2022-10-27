8d290174a3cb1a44266f54bbe0c78ac439ebe823
Olaifa Adedapo <olaifaadedapo@gmail.com>
2022-10-27 11:16:59 +0100

    Node project created

---
+++ index.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+   if(request.url === '/'){
+    let filePath = path.join(__dirname, 'public', request.url === '/' ? 'index.html': request.url)
+    let contentType = getContentType(filePath) || 'text/html'
+    let emptyPagePath = path.join(__dirname, 'public', '404.html')
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+       if (err) {
+          if (err.code === 'ENOENT') {
+            fs.readFile(emptyPagePath, 'utf-8', (err, content) => {
+                response.writeHead(200, {'Content-Type': contentType})
+                response.end(content)
+            })
+          }else {
+            response.writeHead(500)
+            response.end('A sever error has occured')
+          }
+       }
+
+       if (!err) {
+         response.writeHead(200, {'Content-Type': contentType})
+         response.end(content)
+       }
+    })
+   }
+});
+
+const getContentType = (filePath) => {
+    let extname = path.extname(filePath)
+    if (extname === '.js') {
+        return 'text/javascript'
+    }
+    if (extname === '.css') {
+        return 'text/css'
+    }
+    if (extname === '.jpg') {
+        return 'image/jpg'
+    }
+    if (extname === '.png') {
+        return 'image/png'
+    }
+}
+
+// const server = http.createServer((request, response) => {
+//     if(request.url === '/'){
+//           let filePath = path.join(__dirname, 'public', 'index.html')
+//           fs.readFile(filePath, 'utf8', (err, data) => {
+//             response.writeHead(200, {'Content-Type': 'text/html'})
+//             response.end(data)
+//             })
+//         }
+// })
+
+const port = 8000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
